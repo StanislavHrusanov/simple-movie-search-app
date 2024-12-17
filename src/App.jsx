fa600f8750660b857b0c0d8f04bf761cc461feef
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-12-17 14:13:11 +0200

    Adding functionality to fetch movie data and show it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import Movie from "./Movie";
+import Search from "./Search";
 function App() {
+  const [input, setInput] = useState("");
+  const [url, setUrl] = useState("");
+  const [movie, setMovie] = useState({});
+  console.log(movie);
+
+  useEffect(() => {
+    if (url) {
+      (async () => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+          window.alert("Something went wrong!");
+          return;
+        }
+        const resData = await res.json();
+
+        if (resData.Error) {
+          const error = resData.Error;
+          window.alert(error);
+          return;
+        }
+        setMovie(resData);
+        setUrl("");
+      })();
+    }
+  }, [url]);
+
+  function onChangeUrl() {
+    if (input === "") {
+      window.alert("Enter a movie name!");
+      return;
+    }
+    setUrl(`http://www.omdbapi.com/?t=${input}&apikey=3a96370e`);
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.content}>
-        <div className={styles.search}>
-          <input type="text" />
-          <button>Search Movie</button>
-        </div>
-        <Movie />
-      </div>
+      <Search input={input} setInput={setInput} onChangeUrl={onChangeUrl} />
+      {movie.Title ? (
+        <Movie movie={movie} />
+      ) : (
+        <p className={styles.message}>
+          Find information about your favourite movie!
+        </p>
+      )}
     </main>
   );
 }
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,36 +1,33 @@
 import styles from "./Movie.module.css";
 
-export default function Movie() {
+export default function Movie({ movie }) {
   return (
     <div className={styles.container}>
       <div className={styles.image}>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg"
-          alt=""
-        />
+        <img src={movie.Poster} alt={movie.Title} />
       </div>
       <div className={styles.info}>
-        <h2>Gladiator</h2>
+        <h2>{movie.Title}</h2>
         <p>
-          Year:<span>2000</span>
+          Year:<span>{movie.Year}</span>
         </p>
         <p>
-          Runtime:<span>120 min</span>
+          Runtime:<span>{movie.Runtime}</span>
         </p>
         <p>
-          Genre:<span></span>
+          Genre:<span>{movie.Genre}</span>
         </p>
         <p>
-          Director:<span></span>
+          Director:<span>{movie.Director}</span>
         </p>
         <p>
-          Actors:<span></span>
+          Actors:<span>{movie.Actors}</span>
         </p>
         <p>
-          Plot:<span></span>
+          Plot:<span>{movie.Plot}</span>
         </p>
         <p>
-          Imdb Rating:<span></span>
+          Imdb Rating:<span>{movie.imdbRating}</span>
         </p>
       </div>
     </div>
